08a4ce3119e9fda6d96c08c417462673815253b5
Garymarena <garym.arena@gmail.com>
2021-11-24 01:23:43 -0500

    Route from Pokedex to a Pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function Pokemon() {
+export default function Pokemon(props) {
+    // Create a new prop for the navigation route
+    const { navigation, route } = props;
+    console.log(route);
+
     return (
         <View>
             <Text>Estamos en la vista de un Pokémon</Text>
